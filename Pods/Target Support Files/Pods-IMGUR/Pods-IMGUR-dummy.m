309459d0bd276dff94e4ab9f8d6eea959c5c6650
Lucas Bordini <lucasbordini@Lucass-iMac.local>
2020-09-01 12:09:10 -0700

    First Commit

---
+++ Pods/Target Support Files/Pods-IMGUR/Pods-IMGUR-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IMGUR : NSObject
+@end
+@implementation PodsDummy_Pods_IMGUR
+@end
---
+++ Pods/Target Support Files/Pods-IMGURTests/Pods-IMGURTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IMGURTests : NSObject
+@end
+@implementation PodsDummy_Pods_IMGURTests
+@end
