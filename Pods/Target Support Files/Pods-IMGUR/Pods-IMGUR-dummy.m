b97f2d739781fdd64dfe69cfa31f1310498725d3
Lucas Bordini <lucasbordini@Lucass-iMac.local>
2020-09-01 12:12:22 -0700

    Removed pods and podfile.lock

--- Pods/Target Support Files/Pods-IMGUR/Pods-IMGUR-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IMGUR : NSObject
-@end
-@implementation PodsDummy_Pods_IMGUR
-@end
--- Pods/Target Support Files/Pods-IMGURTests/Pods-IMGURTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IMGURTests : NSObject
-@end
-@implementation PodsDummy_Pods_IMGURTests
-@end
